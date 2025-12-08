2629486c68bbfcefe7bb3a5ee50366baba86e166
Yegan Salehi <122101213+Yegan-S@users.noreply.github.com>
2025-12-08 09:15:49 -0800

    Created getNavLinks.ts file as a server component in Next.js

---
+++ frontend/src/lib/getNavLinks.ts
@@ -0,0 +1,14 @@
+export async function getNavLinks() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/navigation-links?sort=order:asc`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    
+  });
+
+  const data = await res.json();
+  return data.data.map((item: any) => ({
+    label: item.label,
+    url: item.url,
+  }));
+}
