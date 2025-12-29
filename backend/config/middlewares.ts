59a8c6b1a402964b43963d11db359aeda6157798
Yegan Salehi <122101213+Yegan-S@users.noreply.github.com>
2025-12-29 07:47:10 -0800

    Updated next.config.ts and middlewares.ts in order to debugging frontend to show images footer and navbar in browser

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -5,8 +5,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://deluxe-lollipop-37c598.netlify.app'],
+      origin: [
+        'https://deluxe-lollipop-37c598.netlify.app',
+        'http://localhost:3000',
+
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
     },
   },
   'strapi::poweredBy',
